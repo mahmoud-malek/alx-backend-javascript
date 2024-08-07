5fc15dcab9cf20b28b6ac74a9d3588e1d03be20e
mahmoud malek <information.mahmoud@gmail.com>
2024-08-08 01:51:19 +0300

    add tasks

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,13 @@
+/* eslint import/no-unresolved: "off", import/extensions: "off" */
+import { uploadPhoto, createUser } from './utils.js';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((responses) => {
+      const [photoResponse, userResponse] = responses;
+      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
