4637e95b9ff50c710706a2679d68436d6fb50fc6
mahmoud malek <information.mahmoud@gmail.com>
2024-08-08 04:23:23 +0300

    add tasks

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -6,6 +6,6 @@ export default async function handleProfileSignup(firstName, lastName, fileName)
     uploadPhoto(fileName)]);
   return results.map((result) => ({
     status: result.status,
-    value: result.value || result.reason,
+    value: result.status === 'fulfilled' ? result.value : result.reason,
   }));
 }
