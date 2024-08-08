ab18a4b5e8a1f776f6746042da7268b300a284ef
mahmoud malek <information.mahmoud@gmail.com>
2024-08-08 04:37:54 +0300

    add tasks

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,13 @@
+/* eslint import/no-unresolved: "off", import/extensions: "off" */
+import { uploadPhoto, signUpUser } from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+    const user = await signUpUser();
+    const photo = await uploadPhoto();
+
+    return { user, photo };
+  } catch (err) {
+    return { user: null, photo: null, error: err };
+  }
+}
