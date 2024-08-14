7d97166eea733e220712fe49f63e5536078c3ed0
mahmoud malek <information.mahmoud@gmail.com>
2024-08-14 03:43:20 +0300

    add task

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,13 @@
+/* eslint  no-shadow: "off" */
+
+export default function updateUniqueItems(map) {
+  if (map instanceof Map) {
+    map.forEach((v, k, map) => {
+      if (v === 1) {
+        map.set(k, 100);
+      }
+    });
+    return map;
+  }
+  throw new Error('Cannot process');
+}
