59d5c3f639b4496f986db3acb8b923fa48c4235a
mahmoud malek <information.mahmoud@gmail.com>
2024-10-01 01:15:13 +0300

    add

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,5 +1,5 @@
 console.log('Welcome to Holberton School, what is your name?');
-
+process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
   if (name !== null) {
