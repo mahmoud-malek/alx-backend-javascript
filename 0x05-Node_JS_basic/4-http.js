9f3e5418fc2511b888ee2b0a7f5bbbbc8362bde1
mahmoud malek <information.mahmoud@gmail.com>
2024-10-01 19:42:16 +0300

    add

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -2,12 +2,12 @@
  * Create a small HTTP server using Node's HTTP module
  */
 
-const { createServer } = require('node:http');
+const http = require('http');
 
 const port = 1245;
-const host = 'localhost';
+const host = '127.0.0.1';
 
-const app = createServer((req, res) => {
+const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.write('Hello Holberton School!');
   res.end();
