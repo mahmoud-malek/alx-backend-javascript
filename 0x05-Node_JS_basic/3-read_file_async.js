581dc6d7aae6630b0a9bfe41fefc309f87ef59fc
mahmoud malek <information.mahmoud@gmail.com>
2024-10-01 17:37:15 +0300

    add

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,33 +1,38 @@
 const fs = require('node:fs/promises');
 
 async function countStudents(path) {
-  try {
-    const data = await fs.readFile(path, 'utf8');
-    const lines = data.split('\n').slice(1).filter((line) => line.trim() !== '');
-    console.log(`Number of students: ${lines.length}`);
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, { encoding: 'utf-8' }).then((data) => {
+      const lines = data.split('\n').slice(1).filter((line) => line.trim() !== '');
+      console.log(`Number of students: ${lines.length}`);
 
-    const students = lines.map((line) => {
-      const [firstname, lastname, age, field] = line.split(',');
-      return {
-        firstname, lastname, age, field,
-      };
-    });
+      const students = lines.map((line) => {
+        const [firstname, lastname, age, field] = line.split(',');
+        return {
+          firstname,
+          lastname,
+          age,
+          field,
+        };
+      });
 
-    const groupedByField = students.reduce((acc, student) => {
-      if (!acc[student.field]) {
-        acc[student.field] = [];
+      const groupedByField = students.reduce((acc, student) => {
+        if (!acc[student.field]) {
+          acc[student.field] = [];
+        }
+        acc[student.field].push(student);
+        return acc;
+      }, {});
+
+      for (const [field, students] of Object.entries(groupedByField)) {
+        const names = students.map((student) => student.firstname).join(', ');
+        console.log(`Number of students in ${field}: ${students.length}. List: ${names}`);
       }
-      acc[student.field].push(student);
-      return acc;
-    }, {});
-
-    for (const [field, students] of Object.entries(groupedByField)) {
-      const names = students.map((student) => student.firstname).join(', ');
-      console.log(`Number of students in ${field}: ${students.length}. List: ${names}`);
-    }
-  } catch (err) {
-    throw new Error('Cannot load the database');
-  }
+      resolve();
+    }).catch(() => {
+      reject(new Error('Cannot load the database'));
+    });
+  });
 }
 
 module.exports = countStudents;
