10e8b9c43fd222be16c87cf18aa2fee01c72606a
mahmoud malek <information.mahmoud@gmail.com>
2024-10-02 04:03:50 +0300

    yes

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,18 @@
+/**
+ *  create a small HTTP server
+ *  using Express module:
+ */
+
+const express = require('express');
+
+const app = express();
+
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(port);
+
+module.exports = app;
